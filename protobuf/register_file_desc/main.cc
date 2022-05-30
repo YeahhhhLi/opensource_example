d8b31f4333ef8b26c5e2b61c08c2e3c95eb35944
ye.li <ye.li@autra.tech>
2022-05-30 12:40:29 +0800

    write/register/new proto dynamically

--- protobuf/register_file_desc/main.cc
+++ protobuf/register_file_desc/main.cc
@@ -6,6 +6,7 @@
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/message.h>
+#include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/util/json_util.h>
 #include "proto/desc/desc.pb.h"
 
@@ -14,8 +15,11 @@ using Descriptor = google::protobuf::Descriptor;
 using FileDescriptor = google::protobuf::FileDescriptor;
 using DescriptorPool = google::protobuf::DescriptorPool;
 
+DescriptorPool pool_g;
+google::protobuf::DynamicMessageFactory factory_g(&pool_g);
+
 void RegisterDesc(const desc::proto::DescProto& desc) {
-  for (int i = 0; i < desc.dependency_count(); ++i) {
+  for (int i = 0; i < desc.dependency_size(); ++i) {
     RegisterDesc(desc.dependency(i));
   }
   google::protobuf::FileDescriptorProto file_desc_proto;
@@ -23,23 +27,54 @@ void RegisterDesc(const desc::proto::DescProto& desc) {
     std::cout << "Fail to parse file desc proto\n";
     return;
   }
-  auto pool = DescriptorPool();
-  ErrorCollector ec;
-  auto file_desc = pool.BuildFileCollectingErrors(file_desc_proto, &ec);
+  // std::cout << "==========file desc proto:\n" << file_desc_proto.Utf8DebugString() << std::endl; 
+  auto file_desc = pool_g.BuildFileCollectingErrors(file_desc_proto, nullptr);
   if (!file_desc) {
     std::cout << "Fail to Build File" << std::endl;
+  } else {
+    std::cout << "Succ to build file, name[" << file_desc->name() << "]" << std::endl;
   }
   return ;
 }
 
 int main(int argc, char** argv) {	
-  param::proto::Params params;
   desc::proto::DescProto desc;
-  std::ifstream ifs("./descriptor.binary", std::ios::in | std::ios::binary);
-  if (!desc.ParseFromIstream(&ifs)) {
-    std::cout << "Fail to read file desc proto\n";
-    return -1;
+  // 注册 message
+  {
+    std::ifstream ifs("../../write_file_desc/descriptor.binary", std::ios::in | std::ios::binary);
+    if (!desc.ParseFromIstream(&ifs)) {
+      std::cout << "Fail to read file desc proto\n";
+      return -1;
+    }
+    RegisterDesc(desc);
   }
-  RegisterDesc(desc);
+  std::string full_name;
+  // 获取 full name
+  {
+    std::ifstream ifs("../../write_file_desc/proto_type.txt", std::ios::in);
+    ifs >> full_name;
+    std::cout << "======full_name[" << full_name << "]" << std::endl;
+  }
+  // new message
+  {
+    auto descriptor = pool_g.FindMessageTypeByName(full_name);
+    if (descriptor == nullptr) {
+      std::cout << "Fail to get message type by name\n";
+      return -1;
+    }
+    auto prototype = factory_g.GetPrototype(descriptor);
+    if (prototype == nullptr) {
+      std::cout << "Fail to get prototype by descriptor\n";
+      return -1;
+    }
+    google::protobuf::Message* msg_ptr = prototype->New();
+    std::ifstream ifs("../../write_file_desc/proto.binary", std::ios::in | std::ios::binary);
+    if (!msg_ptr->ParseFromIstream(&ifs)) {
+      std::cout << "Fail to parse from istream" << std::endl;
+      return -1;
+    }
+    std::cout << "succ pasre proto, debug_str: \n" << msg_ptr->Utf8DebugString() << std::endl; 
+  }
+
   return EXIT_SUCCESS;
 }
--- protobuf/write_file_desc/main.cc
+++ protobuf/write_file_desc/main.cc
@@ -7,7 +7,7 @@
 #include <google/protobuf/descriptor.pb.h>
 #include <google/protobuf/message.h>
 #include <google/protobuf/util/json_util.h>
-#include "proto/params/param.pb.h"
+#include "proto/common/common.pb.h"
 #include "proto/desc/desc.pb.h"
 
 using Message = google::protobuf::Message;
@@ -18,6 +18,7 @@ using DescriptorPool = google::protobuf::DescriptorPool;
 void constructDesc(const FileDescriptor* p_file_descriptor, desc::proto::DescProto* desc_ptr) {
   google::protobuf::FileDescriptorProto file_desc_proto;
   p_file_descriptor->CopyTo(&file_desc_proto);
+  std::cout << "====FileDescProto:" << file_desc_proto.Utf8DebugString() << std::endl;
   if (!file_desc_proto.SerializeToString(desc_ptr->mutable_desc())) {
     std::cout << "Fail to parse file desc proto\n";
     return;
@@ -28,13 +29,29 @@ void constructDesc(const FileDescriptor* p_file_descriptor, desc::proto::DescPro
 }
 
 int main(int argc, char** argv) {	
-  param::proto::Params params;
-  desc::proto::DescProto desc;
-  constructDesc(params.descriptor()->file(), &desc);
-  std::ofstream ofs("./descriptor.binary", std::ios::out | std::ios::binary);
-  if (!desc.SerializeToOstream(&ofs)) {
-    std::cout << "Fail to write file desc proto\n";
-    return -1;
+  common::proto::Payload payload;
+  {
+    std::ofstream ofs("../proto_type.txt", std::ios::out);
+    std::string full_name = payload.descriptor()->full_name();
+    ofs.write((char*)full_name.data(), full_name.size());
+  }
+  payload.set_val_int(123);
+  payload.set_val_str("234");
+  {
+    std::ofstream ofs("../proto.binary", std::ios::out | std::ios::binary);
+    if (!payload.SerializeToOstream(&ofs)) {
+      std::cout << "Fail to write payload proto\n";
+      return -1;
+    }
+  }
+  {
+    desc::proto::DescProto desc;
+    constructDesc(payload.descriptor()->file(), &desc);
+    std::ofstream ofs("../descriptor.binary", std::ios::out | std::ios::binary);
+    if (!desc.SerializeToOstream(&ofs)) {
+      std::cout << "Fail to write file desc proto\n";
+      return -1;
+    }
   }
   return EXIT_SUCCESS;
 }
