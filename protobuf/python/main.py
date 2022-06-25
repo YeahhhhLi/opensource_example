c237a33aba765eb065232713fd2cc534a8f74abc
Yeah <ye.li@autra.tech>
2022-06-25 18:49:39 +0800

    main.py

--- protobuf/python/main.py
+++ protobuf/python/main.py
@@ -1,6 +1,13 @@
 import os
+from proto.common.common_pb2 import Payload
 def main():
-    print("hello world\n")
+    payload = Payload()
+    payload.val_int = 2
+    payload.val_str = "test"
+    payload.val_msg.mock_int = 3
+    payload.val_arr.add().mock_int = 4
+    payload.val_arr.add().mock_int = 5
+    print(payload)
 
 if __name__ == "__main__":
     main()
